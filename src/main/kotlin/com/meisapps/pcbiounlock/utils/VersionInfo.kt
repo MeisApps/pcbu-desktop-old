8d5f5ad7d0bdc38ffbcc4ef3c9c164012596800f
MeisApps <admin@meis-apps.com>
2023-11-30 19:14:54 +0100

    Version 1.2.2

--- src/main/kotlin/com/meisapps/pcbiounlock/utils/VersionInfo.kt
+++ src/main/kotlin/com/meisapps/pcbiounlock/utils/VersionInfo.kt
@@ -5,7 +5,7 @@ import com.meisapps.pcbiounlock.utils.io.Console
 
 object VersionInfo {
     fun getAppVersion(): String {
-        return "1.2.1"
+        return "1.2.2"
     }
 
     fun getProtocolVersion(): String {
