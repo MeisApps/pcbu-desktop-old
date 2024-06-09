058a76609ce73de2f08e8b4a15a2b4e326ee5c91
MeisApps <admin@meis-apps.com>
2024-06-09 19:57:19 +0200

    Update binaries

--- src/main/kotlin/com/meisapps/pcbiounlock/utils/VersionInfo.kt
+++ src/main/kotlin/com/meisapps/pcbiounlock/utils/VersionInfo.kt
@@ -4,7 +4,7 @@ import com.meisapps.pcbiounlock.utils.io.Console
 
 object VersionInfo {
     fun getAppVersion(): String {
-        return "1.5.2"
+        return "1.5.3"
     }
 
     fun getProtocolVersion(): String {
